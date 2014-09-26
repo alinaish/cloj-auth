a00d27e67735498d1784db0457cfc26d94b5e6aa
alinaish <alina.ischenko@genestack.com>
2014-09-26 22:35:56 +0400

    test

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
                  [compojure "1.1.8"]
 		 [joodo "0.10.0"]
 		 [stuarth/clj-oauth2 "0.3.2"]
-		 [speclj "2.2.0"]]
+		 [speclj "2.2.0"]
+		 [hiccup "1.0.2"]
+		 [ring/ring-core "1.3.1"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler test-cloj.handler/app}
   :profiles
--- src/test_cloj/handler.clj
+++ src/test_cloj/handler.clj
@@ -1,12 +1,38 @@
 (ns test-cloj.handler
-  (:require [compojure.core :refer :all]
-            [compojure.handler :as handler]
-            [compojure.route :as route]))
+  (:require
+    [clj-oauth2.client :as oauth2]
+    [joodo.middleware.request :refer [*request*]]
+    [ring.util.response :refer [redirect]]
+    [compojure.core :as cc]
+    [compojure.handler :as handler]
+    [compojure.route :as route]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/resources "/")
-  (route/not-found "Not Found"))
+
+(def login-uri "https://accounts.google.com")
+
+
+(def google-com-oauth2
+  {:authorization-uri (str login-uri "/o/oauth2/auth")
+   :access-token-uri (str login-uri "/o/oauth2/token")
+   :redirect-uri "http://localhost:3000/auth2"
+   :client-id "376988262227-vm27ecnso6hi2k4sgh105k8vr0lqo8br.apps.googleusercontent.com"
+   :client-secret "jW-j51NseajsbEdjP-vJHq-E"
+   :access-query-param :access_token
+   :scope ["https://www.googleapis.com/auth/userinfo.email"]
+   :grant-type "authorization_code"
+   :access-type "online"})
+
+(def auth-req
+  (oauth2/make-auth-request google-com-oauth2))
+
+(defn- google-access-token [request]
+  (oauth2/get-access-token google-com-oauth2 (:params request) auth-req))
+
+(cc/defroutes app-routes
+              (cc/GET "/" [] "Hey how")
+              (cc/GET "/auth" [] (redirect (:uri auth-req)))
+              (cc/GET "/auth2" [] "Nyaaa"))
+
 
 (def app
   (handler/site app-routes))
